7fa99d0c92bd22c416871c617be1f6f8e3dac179
lain <sixtyten@hotmail.com>
2018-12-28 22:17:40 -0600

    Added Review model; extended upload view

--- s7uploads/admin.py
+++ s7uploads/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Upload, Screenshot
+from .models import User, Upload, Screenshot, Review
 
 
 admin.site.register(User)
 admin.site.register(Upload)
 admin.site.register(Screenshot)
+admin.site.register(Review)
--- s7uploads/models.py
+++ s7uploads/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class User(models.Model):
 	username = models.CharField(max_length = 50)
@@ -12,15 +13,12 @@ class Upload(models.Model):
 	url = models.CharField(max_length = 100)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length = 50)
-	description = models.CharField(max_length = 2048)
-	versionNotes = models.CharField(max_length = 512)
+	description = models.TextField(verbose_name="Upload Description")
+	versionNotes = models.TextField(verbose_name="Version Notes")
 	uploadDate = models.DateTimeField('date published')
 	versionNumber = models.DecimalField(max_digits=5, decimal_places = 1)
-	#ratings..
-
 	def indexScreenshot(self):
 		return Screenshot.objects.filter(upload=self)[0].url
-
 	def __str__(self):
 		return self.title
 
@@ -30,3 +28,13 @@ class Screenshot(models.Model):
 	upload = models.ForeignKey(Upload, on_delete=models.CASCADE)
 	def __str__(self):
 		return self.url
+
+class Review(models.Model):
+	title = models.CharField(max_length = 50)
+	text = models.TextField(max_length = 2048, verbose_name="Review Text")
+	upload = models.ForeignKey(Upload, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	pubDate = models.DateTimeField('date published')
+	rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+	def __str__(self):
+		return self.user.username + self.upload.title
