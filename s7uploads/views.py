4c3ee32119c0c9dd41f579bad6bbb91c35101962
alone- <sixtyten@hotmail.com>
2019-03-31 21:01:04 -0500

    fixed file upload

--- s7uploads/views.py
+++ s7uploads/views.py
@@ -75,7 +75,7 @@ def upload_file(request):
 		form = UploadFileForm(request.POST, request.FILES)
 		if form.is_valid():
 			handle_uploaded_file(request.FILES['file'])
-			return HttpResponseRedirect('s7uploads:index')
+			return redirect('s7uploads:index')
 	else:
 		form = UploadFileForm()
 	return render(request, 's7uploads/newupload.html', {'form': form})
