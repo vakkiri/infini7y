dd9894f5efbfbb4e7d11a1f06abdc993837744f9
alone- <sixtyten@hotmail.com>
2019-04-30 09:21:24 -0500

    version notes no longer required field

--- s7uploads/forms.py
+++ s7uploads/forms.py
@@ -19,7 +19,7 @@ class ReviewForm(forms.ModelForm):
 class UploadFileForm(forms.Form):
         title = forms.CharField(max_length=50)
         description = forms.CharField()
-        versionNotes = forms.CharField()
+        versionNotes = forms.CharField(required=False)
         versionNumber = forms.DecimalField(max_digits=5, decimal_places=1)
         file = forms.FileField()
         screenshots = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required=False)
