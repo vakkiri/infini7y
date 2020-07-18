a30e1796b1dfdee97a4475abd1c2650716ec82cf
vakkiri <arkestree@gmail.com>
2020-07-18 12:33:46 -0500

    heroku setup

--- s7/settings.py
+++ s7/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -125,3 +126,6 @@ MEDIA_URL = '/media/'
 # Redirects
 
 LOGOUT_REDIRECT_URL = 'index/'
+
+# heroku setup
+django_heroku.settings(locals())
