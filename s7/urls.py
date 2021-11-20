5727c4fe8ca2f00a463ac7ac21ffe3ac341c6a48
vakkiri <arkestree@gmail.com>
2021-11-20 13:53:48 -0600

    Fix base url

--- s7/urls.py
+++ s7/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-	path('s7uploads/', include('s7uploads.urls')),
+	path('', include('s7uploads.urls')),
 	path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
