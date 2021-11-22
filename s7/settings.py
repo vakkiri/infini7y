be5e1f37a0ef21dddfc2c40697053ed61e09d46f
vakkiri <arkestree@gmail.com>
2021-11-22 11:22:39 -0600

    Don't run prod in debug mode

--- s7/settings.py
+++ s7/settings.py
@@ -12,19 +12,20 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import django_heroku
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = json.load(open("deployment_config.json"))["debug"]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['I7_DJANGO_SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
